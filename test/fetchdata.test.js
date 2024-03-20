0676e58d31eebfd57a094f58d966f6b085ca042f
SanjayKumarrG <sanjukratos33@gmail.com>
2024-03-20 16:39:46 +0530

    add test files

---
+++ test/fetchdata.test.js
@@ -0,0 +1,11 @@
+// import fetchData = require("../src/fetchdata");
+
+test("fetchData returns the correct data", () => {
+    return expect(fetchData()).resolves.toBe(
+      JSON.stringify({
+        name: "Jon Snow",
+        age: 30,
+        email: "jonsnow@housestark.com",
+      })
+    );
+  });
